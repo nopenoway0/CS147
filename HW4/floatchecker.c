200868c129eeca48af386e8176bca77d9080629f
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2016-03-15 14:26:07 -0700

    Prints binary for a byte backwards

--- HW4/floatchecker.c
+++ HW4/floatchecker.c
@@ -7,13 +7,15 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+
 int toBinary(float a);
 
 int main(){
     printf("Entering Program\n");
-float a = -5;
-int a_bin = toBinary(a);
-return 1;
+    float a = -5;
+    int a_bin = toBinary(a);
+    return 1;
 }
 /*
 When grabbing from addresses, make a pointer then navigate through the address using index blocks
@@ -21,22 +23,20 @@ for an unsigned intenger of 256, the first byte[0] will read 128, byte[1] will r
 */
 //Bit shift to right, until number equals 0. If number /= orignal number /2 then that means a 1 dropped off
 //Otherwise, 0 dropped off.
+/*
+ Produces string of the proper binary, but does it backwards
+*/
 int toBinary(float a){
-unsigned int t = 2;
-char* pter = (char*) &t;
-unsigned char b = pter[0];
-int tmp1;
-int tmp2;
-for(int i = 0; i < 8; i++){
-    tmp1 = b / 2;
-    b = b >> 1;
-    tmp2 = (int)b;
-    printf("tmp1 = %d | tmp2 = %d\n", tmp1, tmp2);
-    if(tmp1 != tmp2){
-        printf("1");
+    unsigned int t = 2;
+    char* pter = (char*) &t;
+    unsigned char b = pter[0];
+    int storage;
+    int bin = 0;
+    storage = (int) b;
+    for(int x = 0; x < 8; x++){
+        bin += (storage % 2) * (int)pow(10, x);
+        //printf("%d", storage % 2);
+        storage = storage >> 1;
     }
-    else if(b == 0) printf("0");
-    else printf("0");
-}
     printf("\nExiting Program\n");
 }
