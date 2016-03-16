cdf88867a3477b3ea1899c127098d4967461658a
nopenoway0 <kite670@yahoo.com>
2016-03-16 02:37:09 -0700

    fixed with double powers by storing it into variable

--- HW4/floatchecker.c
+++ HW4/floatchecker.c
@@ -26,10 +26,11 @@ for an unsigned intenger of 256, the first byte[0] will read 128, byte[1] will r
 //Bit shift to right, until number equals 0. If number /= orignal number /2 then that means a 1 dropped off
 //Otherwise, 0 dropped off.
 int toBinary(float a){
-    unsigned int b = 32;
-    char* ptr = &b;
-    printByteBinary((char) ptr[0]);
-    //printByteBinary(a);
+    unsigned int b = 254;
+    char* ptr = b;
+    for(int x = 4; x > 0; x--){
+    printByteBinary((char) ptr);
+    }
     return 1;
 }
 
@@ -38,12 +39,14 @@ void printByteBinary(unsigned char a){
     int storage = a;
     storage = (int) a;
     double power;
+    double log_temp;
     for(int y = 0; y < 8; y++){
         power = pow(10, y);
         bin +=  (storage % 2) * power ;
         storage = storage >> 1;
     }
-    int zeros = 8 - (int)log((bin / 10));
+    log_temp = log10(bin / 10);
+    int zeros = 8 - log_temp;
     if(zeros > 0){
         while(zeros > 1){
         printf("0");
@@ -53,5 +56,5 @@ void printByteBinary(unsigned char a){
     else for(int y = 0; y < 7; y++){
         printf("0");
     }
-    printf("%d", bin);
+    printf("%d\n", bin);
 }
