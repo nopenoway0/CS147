088c62d940d5777766fbc156b071c40fc2f90c29
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2016-03-19 15:07:49 -0700

    V. Before change to storing all results in a char array

--- HW4/floatchecker.c
+++ HW4/floatchecker.c
@@ -53,11 +53,17 @@ void ConvertToBinary(float a){
         sign = 1;
         a *= -1;
     }
-    // Get Whole number and fraction
+    // Get Whole number and fraction set tmp_bit for the calculations without changing whole_number
+    // TODO: can change to not use tmp_bit
     whole_number = (int) a;
     fraction = a - (float) whole_number;
+    tmp_bit = whole_number;
     // Convert Whole Number into Binary, greatest -> least in bin_fraction
-    
+    while(tmp_bit != 0 && count < 32){
+        bin_fraction[count] = tmp_bit % 2;
+        tmp_bit = tmp_bit / 2;
+        count++;
+    }
     //Convert Fraction into binary
     while(fraction != 0 && count < 32){
         fraction *= 2;
@@ -72,8 +78,16 @@ void ConvertToBinary(float a){
         tmp_bit = tmp_bit >> 1;
         exponent++;
     }
+    //Add bias
+    exponent += 127;
+    //Convert exponent to binary
+    tmp_bit = exponent;
+    while(tmp_bit != 0){
+        
+    }
     printf("\n%d|%d|%d|%.4f\n", sign, exponent + 127, whole_number, fraction);
-    count = 0;
+    // Increment count by 1 to not print the 1's place in IEEE notation
+    count = 1;
     while(count < 32){
         printf("%d",bin_fraction[count]);
         count++;
