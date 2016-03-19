ec35031dfd5e53edd78381b30d08ecacac825401
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2016-03-19 14:29:53 -0700

    Finished decimal fraction to binary conversion

--- HW4/floatchecker.c
+++ HW4/floatchecker.c
@@ -14,7 +14,7 @@ void ConvertToBinary(float a);
 
 int main(){
     printf("Entering Program\n");
-    float a = -5;
+    float a = -5.25;
     PrintToBinary(a);
     ConvertToBinary(a);
     printf("\nExiting Program\n");
@@ -40,8 +40,10 @@ void PrintToBinary(float a){;
 void ConvertToBinary(float a){
     unsigned char sign;
     unsigned char exponent = 0;
+    short count = 0;
     int tmp_bit;
-    float remainder;
+    int whole_number;
+    float fraction;
     char bin_fraction[32] = "0";
     // Get sign
     if(a >= 0){
@@ -51,24 +53,29 @@ void ConvertToBinary(float a){
         sign = 1;
         a *= -1;
     }
+    // Get Whole number and fraction
+    whole_number = (int) a;
+    fraction = a - (float) whole_number;
+    // Convert Whole Number into Binary, greatest -> least in bin_fraction
+    
+    //Convert Fraction into binary
+    while(fraction != 0 && count < 32){
+        fraction *= 2;
+        bin_fraction[count] = (char)(int)fraction;
+        if(bin_fraction[count] == 1) fraction -= 1;
+        count++;
+    }
+    //Prep for finding exponent
     tmp_bit = a;
-    // Get fraction
-    while(remainder > 0){
-        remainder = a / 1;
-        remainder = a - remainder;
-        if(remainder > 0){
-            a *= 10;
-        }
-    }
-    remainder = a / 1;
-    remainder = a - remainder;
-    if(remainder > 0){
-        a *= 10;
-    }
     // Find the exponent without bias
     while(tmp_bit > 1){
         tmp_bit = tmp_bit >> 1;
         exponent++;
     }
-    printf("\n%d|%d|", sign, exponent + 127);
+    printf("\n%d|%d|%d|%.4f\n", sign, exponent + 127, whole_number, fraction);
+    count = 0;
+    while(count < 32){
+        printf("%d",bin_fraction[count]);
+        count++;
+    }
 }
