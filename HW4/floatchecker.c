31c377aac34376450be1a6e4bdebf51f4b42c134
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2016-03-22 20:51:09 -0700

    fixed warning crashing when from terminal

--- HW4/floatchecker.c
+++ HW4/floatchecker.c
@@ -15,13 +15,16 @@ float stof(const char* s);
 
 int main(int argc, char**argv){
     float  a;
-    if(argc >= 2){
+    if(argc == 2){
         printf("Entering Program with arguments %s|%f\n", argv[0], stof(argv[1]));
         a = stof(argv[1]);
     }
-    if(argc < 2){
+    else if(argc < 2){
             printf("Enter float to convert: ");
             scanf("%f",&a);
+    }
+    else{
+        printf("Improper input format\n");
     }
     printf("Printing bits from memory: ");
     PrintToBinary(a);
@@ -36,7 +39,7 @@ int main(int argc, char**argv){
 //Otherwise, 0 dropped off.
 void PrintToBinary(float a){;
     int num_loops = sizeof(a) * 8 - 1;  // Sets the amount of bit shfts
-    int* ptr = &a;                      // Have int point to the address of the float
+    int* ptr = (int*)&a;                      // Have int point to the address of the float
     int bit;
 
     while(num_loops > 0){
