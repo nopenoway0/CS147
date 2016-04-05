b66b582dda0ae9f0a0d609fe8a9acf94dd192eb7
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2016-04-04 20:40:30 -0700

    Update

--- HW5/hw5.c
+++ HW5/hw5.c
@@ -1,14 +1,16 @@
 //
 //  hw5.c
-//  
+//  Assumes that the system is 32-bit because mips is 32-bit
 //
 //  Created by Benjamin on 4/2/16.
 //  Transfer in memory using integer copies when possible, otherwise default
 //  to byte copies. Account for when memory alignment is not to size of integers
 //
-//  Read by address, or by chunk of allocation? If it is a half, don't load int?
+//  Read by address, or by chunk of allocation? If it is a half, don't load int
+//  Determine by the size when to read a byte. If the it ends on an address not divisible by 4 then it can be used
+//  or read by a byte
 
-int memmov(void* source, void* dest); // Uses integer copy instead of byte
+int memmov(void* source, void* dest, int length_of_mem); // Uses integer copy instead of byte
 
 int main(){
     int number = 20;
