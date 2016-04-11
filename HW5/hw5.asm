42d90ff3b9f8bfbbd0650090dc11cfd5f9c2f4cf
-v <benjamin@Benjamins-MacBook-Air.local>
2016-04-11 03:15:08 -0700

    Print_char_array created

--- HW5/hw5.asm
+++ HW5/hw5.asm
@@ -5,7 +5,7 @@
 .end_macro
 
 .data
-.align 2
+.align 0
 array1: .word 15 23 19 94 1995 22
 array2: .word 0 0 0 0 0 0
 array3: .byte 2 3 4 5 6 7
@@ -37,6 +37,14 @@ main:
 	la	$a0, array2
 	la	$a1, 6
 	jal 	print_int_array
+	
+	la	$a0, array3
+	la	$a1, 6
+	jal 	print_char_array
+	
+	la	$a0, array4
+	la	$a1, 6
+	jal 	print_char_array
 	
 	exit
 
@@ -98,7 +106,7 @@ pa_loop:
 	lw	$t3, 0($t4)
 	
 	move	$a0, $t3
-	li	$v0, 1
+	li	$v0, 11
 	syscall
 	
 	lb	$a0, space
@@ -109,6 +117,44 @@ pa_loop:
 	addi	$t1, $t1, 1
 	j	pa_loop
 pa_loop_end:
+	lb	$a0, newline
+	li	$v0, 11
+	syscall
+	
+	lw	$v0, 0($sp)
+	lw	$fp, 4($sp)
+	lw	$a0, 8($sp)
+	addi	$sp, $sp, 12
+	jr	$ra
+
+# a0 is address of the start of the array. a1 is the length	
+print_char_array:
+	move	$t0, $a1
+	li	$t1, 0
+	addi	$sp, $sp, -12
+	sw	$v0, 0($sp)
+	sw	$fp, 4($sp)
+	sw	$a0, 8($sp)
+	addi	$fp, $sp, 12
+pc_loop:
+	slt	$t2, $t1, $a1
+	beqz	$t2, pc_loop_end
+	sll	$t3, $t1, 0
+	add	$t4, $t3, $a0
+	lb	$t3, 0($t4)
+	
+	move	$a0, $t3
+	li	$v0, 11
+	syscall
+	
+	lb	$a0, space
+	li	$v0, 11
+	syscall
+	
+	lw	$a0, 8($sp)
+	addi	$t1, $t1, 1
+	j	pc_loop
+pc_loop_end:
 	lb	$a0, newline
 	li	$v0, 11
 	syscall
